583edc9a24c366c176d4be0070e6d5616f7d1c80
AKLC\gausdew <wade.gausden@aucklandcouncil.govt.nz>
2023-07-14 11:59:46 +1200

    Initial Commit

---
+++ .storybook/preview.cjs
@@ -0,0 +1,12 @@
+import '../themes/style.scss';
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ src/stories/Button.tsx
@@ -0,0 +1,9 @@
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = () => {
+  return (
+    <div className="has-background"></div>
+  );
+};
