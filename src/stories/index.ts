c162b0db90a5cd3226381ca825a168db9c9a5b40
AKLC\gausdew <wade.gausden@aucklandcouncil.govt.nz>
2023-07-14 12:10:32 +1200

    Add vite bundler

---
+++ src/stories/index.ts
@@ -0,0 +1,3 @@
+import '../../themes/style.scss';
+
+export * from './Button';
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build : {
+    lib: {
+      entry: ['./src/stories/index.ts'],
+      name: 'ComponentLibrary',
+      fileName: (format) => `index.js`,
+      formats : ['umd'],
+    }, 
+  }, 
   plugins: [react()],
 })
